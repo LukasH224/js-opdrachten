3b6a7714744fdfc4f7fa6ad6d98df5574524e26b
LukasH224 <302425467@student.rocmondriaan.nl>
2024-09-10 10:26:48 +0200

    js

---
+++ main.js
@@ -0,0 +1,17 @@
+const userName = "Lukas"
+let temperatuur = 20;
+     console.log(userName);
+     alert(`de temperatuur is ${temperatuur} graden`);
+     temperatuur += 5;
+      console.log (temperatuur);
+
+
+// opdr interatie
+const askName = prompt(`wat is je naam`);
+alert (askName);
+
+const Cookies = confirm(`accept je de cookies?`);
+console.log(Cookies);
+
+const myCity = 'Den Haag';
+alert('Ik woon in ' + myCity);
